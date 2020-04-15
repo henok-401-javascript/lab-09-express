b518881ccf83e2037591a3f331c4baf22619f9bf
henok Gebremedhn <henoklwi@gmail.com>
2020-04-14 21:22:20 -0700

    finished lab-09

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const app = require('./lib/server.js');
 const mongoose = require('mongoose');
 require('dotenv').config();
 const MONGODB_URI = process.env.MONGODB_URI;
-const port = process.env.PORT || 3000;
+
+const port = process.env.PORT;
 
 mongoose.connection.once('open', () => {
   console.log('mongo db is on');
